4f53613027de6f8602b9d835fedfab7546cb2137
maria liza <mariamartinez@pursuit.org>
2020-07-14 18:15:24 -0400

    correct methods

--- nonDSAStandards.js
+++ nonDSAStandards.js
@@ -13,36 +13,24 @@ console.log(findSum([1, 2, 3, 4, 5]));
 
 // Using reduce write a function that returns the sum of the array.
 
-const reduceSum = (arr) => {
-  return arr.reduce((acc, currEl) => {
+return  [1, 2, 3, 4, 5].reduce((acc, currEl) => {
     acc += currEl;
     return acc;
-  }, 0);
-};
-console.log(reduceSum([1, 2, 3, 4, 5]));
+  }, 0)
+
 
 // Using filter return an array of only values greater than or equal to 3.
 
-const three = (arr, target = 3) => {
-  let newArr = [];
-  arr.filter((el) => {
-    if (el >= target) {
-      newArr.push(el);
-    }
+return [1, 2, 3, 4, 5].filter((el) => {
+    let target = 3;
+    return el >= target;
   });
-  return newArr;
-};
-console.log(three([1, 2, 3, 4, 5]));
 
 // Using map return the array [2, 4, 6, 8, 10].
 
-const doubleArr = (arr) => {
-  let doubled = [];
-  arr.map((el) => {
-    doubled.push(el * 2);
+const arr2 = [1, 2, 3, 4, 5];
+return arr2.map((el) => {
+    return el * 2;
   });
-  return doubled;
-};
-console.log(doubleArr([1, 2, 3, 4, 5]))
 
 // If you could solve these I could mark you off on two standards which would bring you above 85% percent.
